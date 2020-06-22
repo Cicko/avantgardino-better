319ab5064332b24ec373eb6c28b48eea2c5e9ff1
Rudolf <rudolfcickodev@gmail.com>
2020-06-22 16:59:42 +0200

    Fixing stuff

--- scrapper.py
+++ scrapper.py
@@ -6,16 +6,22 @@ URL = 'https://www.avantgardino.cz/menu/'
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
-table = soup.find("div", class_="dennimenu")
+data = soup.find("div", class_="dennimenu")
+
+#titles = data.find_all('h3')
+
+print(data)
 
 output_rows = []
-for table_row in table.findAll('tr'):
-    columns = table_row.findAll('td')
-    output_row = []
-    for column in columns:
-        output_row.append(column.text)
-    output_rows.append(output_row)
-    
+for table_row in data.findAll('tr'):
+	columns = table_row.findAll('td')
+	output_row = []
+	for column in columns:
+		output_row.append(column.text)
+	output_rows.append(output_row)
+	output_rows.append('NEXT_TYPE')
+
+
 with open('data/menu.csv', 'w') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerows(output_rows)
+	writer = csv.writer(csvfile)
+	writer.writerows(output_rows)
